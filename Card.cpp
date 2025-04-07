13f0e54f2902da8fff1638ae65e0464012a237b0
Nick-Altland <7930610+nick-altland@users.noreply.github.com>
2025-04-07 15:01:23 -0400

    Created Card Class

---
+++ Card.cpp
@@ -0,0 +1,15 @@
+/**
+ * Card class source file
+ * 
+ * Cards have a SUIT, a VALUE, and an ENHANCEMENT.
+ * 
+ * Example: Eight (eight chips) of diamonds (suit = diamonds) with enhancment mult (+4 mult) and no red seal
+ * 
+ * With this in mind, a card needs chip value, suit, enhancement, and red seal (boolean)
+*/
+
+class Card{
+
+    
+
+};
